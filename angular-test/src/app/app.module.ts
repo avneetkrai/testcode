30d624b9d1614d603dc0a4b91bdd3f2216a71747
avneetkrai <69210610+avneetkrai@users.noreply.github.com>
2020-08-08 19:58:34 +0530

    Adding files

--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.routing';
+import { NavbarModule } from './common/navbar/navbar.module';
+import { ViewScreenComponent } from './dashboard/view/viewscreen.component';
+import { AddScreenComponent } from './dashboard/add/addscreen.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './common/footer/footer.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ViewScreenComponent,
+    AddScreenComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    NavbarModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
